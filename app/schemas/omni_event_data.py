bed7f0650eca0ce023f6fdebd5802412fc83a2bc
EricLicea-Projects <elicea.2000@gmail.com>
2026-02-24 00:39:45 -0800

    Created ne Pydantic models, and updated GA Service module to use the new OmniEvent Schema.

---
+++ app/schemas/omni_event_data.py
@@ -0,0 +1,45 @@
+from typing import List
+from pydantic import BaseModel, HttpUrl
+from datetime import datetime
+
+class OmniEventData(BaseModel):
+    event: dict
+    players: dict
+    standings: dict
+    rounds: List
+
+class OmniEvent(BaseModel):
+    event_id: int
+    ranked: bool
+    swiss_match_config: str
+    swiss_rounds: int
+    start_at: datetime
+    url: HttpUrl
+
+class OmniPlayer(BaseModel):
+    player_id: int
+    username: str
+    country: str
+    player_cp: int
+    player_rank: int
+    player_emblem: str
+
+class OmniEventStanding(BaseModel):
+    event_id: int
+    player_id: int
+    placement: int
+    wins: int
+    losses: int
+    ties: int
+    byes: int
+    score: int
+
+class OmniEventParticipant(BaseModel):
+    event_id: int
+    player_id: int
+    round_id: int
+    pairing_id: int
+    dropped: bool
+    score: int
+    status: str
+    elo_change: float
--- app/services/grand_archive_service.py
+++ app/services/grand_archive_service.py
@@ -2,11 +2,12 @@ import httpx
 from asyncio import gather
 
 from app.clients import grand_archive_api as ga
+from app.schemas.omni_event_data import OmniEventData
 
 async def get_omni_local_event(
         client: httpx.AsyncClient,
         event_id: int,
-)-> dict:
+)-> OmniEventData:
     event, players, standings, *rounds = await gather(
         ga.get_omni_event(client,event_id),
         ga.get_omni_event_players(client, event_id),
